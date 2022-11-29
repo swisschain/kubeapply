fc43ea4e6e6cf9c5372bea59d8ecc97a2aa37927
Dvin <dvyu@yandex.ru>
2022-11-29 15:52:41 +0500

    entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,24 +1,25 @@
-#!/bin/bash
+#!/bin/sh
 
 # exit when any command fails
 set -e
 
 # run checks
-LAST_COMMIT=$(git --no-pager log | head -1 | awk -F"it " '{print $2}')
+#LAST_COMMIT=$(git --no-pager log | head -1 | awk -F"it " '{print $2}')
 #echo LAST_COMMIT=$LAST_COMMIT
+find / -name bash
 
-for i in $((
-           for i in $(git --no-pager show $LAST_COMMIT | grep ^--- | awk -F"a/" '{print $2}');do echo $i; done
-           for i in $(git --no-pager show $LAST_COMMIT | grep ^+++ | grep -v /dev/null | awk -F"b/" '{print $2}');do echo $i; done
-           ) | sort | uniq )
-do
-  if [ -f $i ];then
-    if grep "kind: Deployment\|kind: ConfigMap\|kind: Service\|kind: Secret" $i > /dev/null 2>&1;then
-      echo $i
-    fi
-  fi
-done
+#for i in $((
+#           for i in $(git --no-pager show $LAST_COMMIT | grep ^--- | awk -F"a/" '{print $2}');do echo $i; done
+#           for i in $(git --no-pager show $LAST_COMMIT | grep ^+++ | grep -v /dev/null | awk -F"b/" '{print $2}');do echo $i; done
+#           ) | sort | uniq )
+#do
+#  if [ -f $i ];then
+#    if grep "kind: Deployment\|kind: ConfigMap\|kind: Service\|kind: Secret" $i > /dev/null 2>&1;then
+#      echo $i
+#    fi
+#  fi
+#done
 # apply
-if [ "$APPLY" = "true" ];then
-  echo APPLY
-fi
+#if [ "$APPLY" = "true" ];then
+#  echo APPLY
+#fi
