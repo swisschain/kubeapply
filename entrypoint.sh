230eed16f49933d7e752c252d991244c533dd271
Dvin <dvyu@yandex.ru>
2022-11-29 16:56:51 +0500

    entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,7 +4,7 @@
 set -e
 
 # run checks
-git config --global --add safe.directory /github/workspace
+#git config --global --add safe.directory /github/workspace
 LAST_COMMIT=$(git --no-pager log | head -1 | awk -F"it " '{print $2}')
 echo LAST_COMMIT=$LAST_COMMIT
 git --no-pager show $LAST_COMMIT
