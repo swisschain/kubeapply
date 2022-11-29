c35dae403def4cd035a7ead8c3777e3fa996d42a
Dvin <dvyu@yandex.ru>
2022-11-29 15:38:20 +0500

    add entrypoint.sh

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
 # kubeapply
+Apply all changed in commit to kunernetes
+# example of usage in pipeline
+```
+name: Deploy test
+
+on:
+  push:
+    branches: [ master ]
+#    paths:
+#    - 'Lykke-Jobs-Mailerlite/**'
+#    - 'Lykke-Service-Mailerlite/**'
+
+jobs:
+  deploy:
+    name: deploy
+    runs-on: ubuntu-latest
+    steps:
+    - uses: actions/checkout@v2
+    - name: deploy
+      uses: swisschain/kubeapply@master
+      env:
+        KUBE_CONFIG_DATA: ${{ secrets.LYKKE_TEST_KUBE_CONFIG_DATA }}
+```
+
---
+++ entrypoint.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+# exit when any command fails
+set -e
+
+# run checks
+LAST_COMMIT=$(git --no-pager log | head -1 | awk -F"it " '{print $2}')
+#echo LAST_COMMIT=$LAST_COMMIT
+
+for i in $((
+           for i in $(git --no-pager show $LAST_COMMIT | grep ^--- | awk -F"a/" '{print $2}');do echo $i; done
+           for i in $(git --no-pager show $LAST_COMMIT | grep ^+++ | grep -v /dev/null | awk -F"b/" '{print $2}');do echo $i; done
+           ) | sort | uniq )
+do
+  if [ -f $i ];then
+    if grep "kind: Deployment\|kind: ConfigMap\|kind: Service\|kind: Secret" $i > /dev/null 2>&1;then
+      echo $i
+    fi
+  fi
+done
+# apply
+if [ "$APPLY" = "true" ];then
+  echo APPLY
+fi
