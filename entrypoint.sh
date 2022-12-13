b9bbdc39c6d8f0343016bf39ff44229979c433d8
Dvin <dvyu@yandex.ru>
2022-12-13 17:37:43 +0500

    entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,7 +3,47 @@
 # exit when any command fails
 set -e
 
+# functions
+check_for_kube_file() {
+  if grep "kind: Deployment\|kind: ConfigMap\|kind: Service\|kind: Secret" $FILE > /dev/null 2>&1;then
+    return 0
+  else
+    return 1
+  fi
+}
+
+check_and_apply() {
+if check_for_kube_file;then
+  echo dry run client
+  kubectl apply --dry-run='client' -f $FILE
+  echo dry run server
+  kubectl apply --dry-run='server' -f $FILE
+  if [ "$APPLY" = "true" ];then
+    echo APPLY $FILE
+    kubectl apply -f $FILE
+  fi
+else
+  echo $FILE not valid kube file
+fi
+}
+
+check_and_delete() {
+if check_for_kube_file;then
+  echo dry run client
+  kubectl delete --dry-run='client' -f $FILE
+  echo dry run server
+  kubectl delete --dry-run='server' -f $FILE
+  if [ "$APPLY" = "true" ];then
+    echo APPLY $FILE
+    kubectl delete -f $FILE
+  fi
+else
+  echo $FILE not valid kube file
+fi
+}
 # run checks
+DELETED_FILES=/tmp/deleted_files
+echo > $DELETED_FILES
 echo get kube config
 echo "$KUBE_CONFIG_DATA" | base64 -d > /tmp/config
 export KUBECONFIG=/tmp/config
@@ -11,6 +51,7 @@ echo get kube nodes
 kubectl get nodes
 echo get git changes
 git config --global --add safe.directory /github/workspace
+#LAST_COMMIT=$(git --no-pager log | head -1 | awk -F"it " '{print $2}')
 LAST_COMMIT=$(git --no-pager log | head -1 | sed 's#commit ##g')
 echo LAST_COMMIT=$LAST_COMMIT
 #
@@ -19,18 +60,28 @@ for FILE in $((
            for GIA in $(git --no-pager show $LAST_COMMIT | grep ^+++ | grep -v /dev/null | sed 's#+++ b/##g');do echo $GIA; done
            ) | sort | uniq )
 do
+  echo -=[ processing $FILE ]=-
   if [ -f $FILE ];then
-    if grep "kind: Deployment\|kind: ConfigMap\|kind: Service\|kind: Secret" $FILE > /dev/null 2>&1;then
-      echo check $FILE
-      echo dry run client
-      kubectl apply --dry-run='client' -f $FILE
-      echo dry run server
-      kubectl apply --dry-run='server' -f $FILE
-      if [ "$APPLY" = "true" ];then
-        echo APPLY $FILE
-        kubectl apply -f $FILE
-      fi
-    fi
+    check_and_apply
+  else
+    echo file $FILE looks like deleted - will check
+    echo $FILE >> $DELETED_FILES
   fi
 done
+# 
+echo check for deleted files
+#PREV_COMMIT=$(git --no-pager log | grep ^commit | head -2 | tail -1 | awk -F"it " '{print $2}')
+PREV_COMMIT=$(git --no-pager log | grep ^commit | head -2 | tail -1 | sed 's#commit ##g')
+echo PREV_COMMIT=$PREV_COMMIT
+git checkout $PREV_COMMIT
+for FILE in $(cat $DELETED_FILES)
+do
+  echo -=[ processing $FILE ]=-
+  if [ -f $FILE ];then
+    check_and_delete
+  else
+    echo can not find $FILE
+  fi
+done
+rm /tmp/deleted_files
 rm /tmp/config
